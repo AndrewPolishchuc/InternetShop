3071d0e67b43d56e7eb21375f3ffddf148122486
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-09-03 15:16:33 +0300

    Correct solution

--- src/main/java/com/internet/shop/model/Order.java
+++ src/main/java/com/internet/shop/model/Order.java
@@ -7,6 +7,11 @@ public class Order {
     private List<Product> products;
     private Long userId;
 
+    public Order(List<Product> products, Long userId) {
+        this.products = products;
+        this.userId = userId;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
--- src/main/java/com/internet/shop/model/User.java
+++ src/main/java/com/internet/shop/model/User.java
@@ -6,6 +6,40 @@ public class User {
     private String login;
     private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String name, String login, String password) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
