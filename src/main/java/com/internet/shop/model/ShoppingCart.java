988c88a28bd99e084bc694984438e403505a7345
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-09-03 17:37:58 +0300

    Added constructors and change initialization fields.

--- src/main/java/com/internet/shop/model/ShoppingCart.java
+++ src/main/java/com/internet/shop/model/ShoppingCart.java
@@ -1,11 +1,17 @@
 package com.internet.shop.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
     private Long id;
     private List<Product> products;
     private Long userId;
+
+    public ShoppingCart(Long userId) {
+        products = new ArrayList<>();
+        this.userId = userId;
+    }
 
     public void setId(Long id) {
         this.id = id;
