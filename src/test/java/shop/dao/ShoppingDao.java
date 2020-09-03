d548895f7615aad5889788f3a9a5070bc2ec8eb6
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-09-03 10:51:51 +0300

    Rollback

---
+++ src/test/java/shop/dao/ShoppingDao.java
@@ -0,0 +1,18 @@
+package shop.dao;
+
+import com.internet.shop.model.ShoppingCart;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ShoppingDao {
+    ShoppingCart create(ShoppingCart shoppingCart);
+
+    Optional<ShoppingCart> get(Long id);
+
+    List<ShoppingCart> getAll();
+
+    ShoppingCart update(ShoppingCart shoppingCart);
+
+    boolean delete(Long id);
+}
