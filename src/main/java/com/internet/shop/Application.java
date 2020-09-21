1eac20c521d536545726e7ccab5d95d1fc02eac0
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-09-21 14:45:45 +0300

    Change implementation on the interface

--- src/main/java/com/internet/shop/Application.java
+++ src/main/java/com/internet/shop/Application.java
@@ -1,12 +1,14 @@
 package com.internet.shop;
 
+import com.internet.shop.dao.ProductDao;
 import com.internet.shop.dao.jdbc.ProductDaoJdbcImpl;
+import com.internet.shop.lib.Injector;
 import com.internet.shop.model.Product;
 
 public class Application {
 
     public static void main(String[] args) {
-        ProductDaoJdbcImpl dao = new ProductDaoJdbcImpl();
+        ProductDao dao = new ProductDaoJdbcImpl();
         Product orange = new Product("orange",180);
         Product pineapple = new Product("pineapple",300);
         System.out.println(dao.getAll());
