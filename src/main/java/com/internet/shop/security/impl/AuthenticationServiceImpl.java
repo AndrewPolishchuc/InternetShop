13ffa2526ac034a707475c6adbaba775365d1252
AndrewPolishchuk <olkftrylk2@gmail.com>
2020-09-29 12:12:48 +0300

    Added solution

--- src/main/java/com/internet/shop/security/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/internet/shop/security/impl/AuthenticationServiceImpl.java
@@ -6,6 +6,8 @@ import com.internet.shop.lib.Service;
 import com.internet.shop.model.User;
 import com.internet.shop.security.AuthenticationService;
 import com.internet.shop.service.UserService;
+import com.internet.shop.util.HashUtil;
+
 import java.util.Optional;
 
 @Service
@@ -16,9 +18,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String login, String password) throws AuthenticationException {
         Optional<User> user = userService.findByLogin(login);
-        if (user.isPresent() && user.get().getPassword().equals(password)) {
+        if (user.isPresent() && isValidPassword(password,
+                user.get().getSalt(), user.get().getPassword())) {
             return user.get();
         }
         throw new AuthenticationException("Incorrect username or password");
+    }
+
+    private boolean isValidPassword(String password, byte[] userSalt, String userFromDbPassword) {
+        return HashUtil.hashPassword(password, userSalt).equals(userFromDbPassword);
     }
 }
